07ff152ae97c4259e98537b2145941f0ebe52743
OnlyCharacters <yuxian9630@gmail.com>
2022-01-02 22:44:07 +0800

    适配新的登录系统

---
+++ BJFU-AutoConnect.py
@@ -0,0 +1,59 @@
+import requests
+import base64
+
+def cutString(str, s_start, s_end):
+    pos_start = str.find(s_start)
+    str_temp = str[pos_start + len(s_start):-1]
+    pos_end = str_temp.find(s_end)
+    return str_temp[:pos_end]
+
+username = ''
+password = ''
+with open('xyw.txt','r') as f:
+    lines=f.readlines()
+    username = lines[0].rstrip()
+    password = lines[1].rstrip()
+
+# dor_wifi
+r = requests.get('http://202.204.122.1/index.jsp')
+wifi_url = r.url
+p1 = cutString(wifi_url, 'p1=', '&')
+p2 = cutString(wifi_url, 'p2=', '&')
+p3 = cutString(wifi_url, 'p3=', '&')
+dataToPost1 = {
+    "p1": 3,
+    "p2": p2,
+    "p3": p3,
+    "p4": "0",
+    "p5": username,
+    "p6": password,
+    "p7": "0",
+    "PtUser": username,
+    "PtPwd": password,
+    "PtButton": "logon",
+}
+r = requests.post('http://192.168.55.2:8800/ext_captive_portal.html', data=dataToPost1)
+
+
+r = requests.get('http://10.1.1.10/')
+text = r.text
+ip = cutString(text, 'v46ip=\'', '\'')
+pass_base = base64.b64encode(password.encode()).decode()
+dataToGet = {
+    'callback': 'dr1004',
+    'login_method': 1,
+    'user_account': username,
+    'user_password': pass_base,
+    'wlan_user_ip': ip,
+    'wlan_user_ipv6': '',
+    'wlan_user_mac': '000000000000',
+    'wlan_ac_ip': '',
+    'wlan_ac_name': '',
+    'jsVersion': '4.1.3',
+    'terminal_type': 1,
+    'type': 1,
+    'lang': 'en',
+    'v': 1218,
+    'lang': 'en',
+}
+r = requests.get('http://10.1.1.10:801/eportal/portal/custom/auth', params=dataToGet)
--- README.md
+++ README.md
@@ -1,25 +1,13 @@
 # BJFU校园网自动连接
-## 可以直接执行.py，前提是安装python3，并且安装了requests包
-如果不愿意安装python3或者不想安装requests包，可以去[release](https://github.com/OnlyCharacters/BJFU-AutoConnect/releases)找打包好的可执行文件
 
-## xyw.txt 里第一行填写学号，第二行填写校园网密码
-connect.py 用于有线
+- 使用方法：
 
-connect_wifi.py 用于连接宿舍区域的bjfu-wifi
+  1. 前往 release 页面下载 BJFU-AutoConnect.zip
+  2. 下载完成后，解压
+  3. 打开 xyw.txt，在第一行填上学号，第二行填上登录的密码，保存退出
+  4. 双击 BJFU-AutoConnect.exe 运行即可
 
-disconnect.py 用于断开连接
+  如果有报毒情况，把文件还原添加信任即可。
 
-connect_wifi2.py 用于连接学研、食堂（登录界面长这个样的）区域的bjfu-wifi
+  源代码是公开的，你也可以自行安装 python3，并安装 requests 包，然后直接运行源代码。
 
-
-话说为什么学校有两个WiFi认证？
-
-学研、西配、食堂使用一套WiFi认证，宿舍使用另一套WiFi认证，为什么？
-
-- 2021-12-31
-  学校似乎准备了新的登陆界面，但目前此脚本依然可用
-
-- 2022-01-01
-  信息中心更新完之后，有线（connect.py）g了，connect_wifi.py和connect_wifi2.py暂时可用，disconnect.py也g了
-
-  新的登录逻辑真复杂
