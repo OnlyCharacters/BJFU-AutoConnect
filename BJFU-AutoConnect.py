43dfc666db839a98330c35dabc814ea7fa0418ad
OnlyCharacters <yuxian9630@gmail.com>
2022-01-06 15:24:24 +0800

    删除宿舍判断，因为校园网登录终于统一了

--- BJFU-AutoConnect.py
+++ BJFU-AutoConnect.py
@@ -1,6 +1,6 @@
 import requests
 import base64
-import socket
+# import socket
 
 def cutString(str, s_start, s_end):
     pos_start = str.find(s_start)
@@ -15,28 +15,28 @@ with open('xyw.txt','r') as f:
     username = lines[0].rstrip()
     password = lines[1].rstrip()
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-result = sock.connect_ex(('192.168.55.2', 8800))
-if result == 0:
-    # dor_wifi
-    r = requests.get('http://202.204.122.1/index.jsp')
-    wifi_url = r.url
-    p1 = cutString(wifi_url, 'p1=', '&')
-    p2 = cutString(wifi_url, 'p2=', '&')
-    p3 = cutString(wifi_url, 'p3=', '&')
-    dataToPost1 = {
-        "p1": 3,
-        "p2": p2,
-        "p3": p3,
-        "p4": "0",
-        "p5": username,
-        "p6": password,
-        "p7": "0",
-        "PtUser": username,
-        "PtPwd": password,
-        "PtButton": "logon",
-    }
-    r = requests.post('http://192.168.55.2:8800/ext_captive_portal.html', data=dataToPost1)
+# sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# result = sock.connect_ex(('192.168.55.2', 8800))
+# if result == 0:
+#     # dor_wifi
+#     r = requests.get('http://202.204.122.1/index.jsp')
+#     wifi_url = r.url
+#     p1 = cutString(wifi_url, 'p1=', '&')
+#     p2 = cutString(wifi_url, 'p2=', '&')
+#     p3 = cutString(wifi_url, 'p3=', '&')
+#     dataToPost1 = {
+#         "p1": 3,
+#         "p2": p2,
+#         "p3": p3,
+#         "p4": "0",
+#         "p5": username,
+#         "p6": password,
+#         "p7": "0",
+#         "PtUser": username,
+#         "PtPwd": password,
+#         "PtButton": "logon",
+#     }
+#     r = requests.post('http://192.168.55.2:8800/ext_captive_portal.html', data=dataToPost1)
 
 r = requests.get('http://10.1.1.10/')
 text = r.text
